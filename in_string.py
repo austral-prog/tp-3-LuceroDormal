0b92d8c52f014b71863614093d1845f148d78b91
LuceroDormal <ldormal@mail.austral.edu.ar>
2025-04-04 14:52:25 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -7,10 +7,8 @@ def check_vowels():
 	o = "o"
 	u = "u"
 
-	print(f"Contiene {a}: {"a" in nombre}")
-	print(f"Contiene {e}: {"e" in nombre}")
-	print(f"Contiene {i}: {"i" in nombre}")
-	print(f"Contiene {o}: {"o" in nombre}")
-	print(f"Contiene {u}: {"u" in nombre}")
-
-
+	print(f"Contiene a: {"a" in nombre}")
+	print(f"Contiene e: {"e" in nombre}")
+	print(f"Contiene i: {"i" in nombre}")
+	print(f"Contiene o: {"o" in nombre}")
+	print(f"Contiene u: {"u" in nombre}")
